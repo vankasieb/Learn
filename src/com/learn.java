665479624ec8807902275d1f244faf8ccc85d7a0
vankasieb <vankasieb@gmail.com>
2015-03-09 13:53:55 +0200

    123

---
+++ src/com/learn.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by ivan.balaban on 09.03.2015.
+ */
+public class learn {
+    public static void main(String[] args) {
+        List<String> myAList = new ArrayList<String>(Arrays.asList("four", "five"));
+        System.out.println(myAList);
+        myAList.add("one");
+        myAList.add("two");
+        myAList.add("three");
+
+        for (String s:myAList) {
+            System.out.println(s);
+        }
+        int two = myAList.indexOf("two");
+        System.out.println("two is " + (two+1) + " of " + myAList.size());
+
+        //System.out.println("Hello");
+
+    }
+}
